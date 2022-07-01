46e75d3edb8591e305e7f40e8c5c24b14284b2f5
ccristianoviedo <ccristianoviedo@gmail.com>
2022-07-01 18:38:21 -0300

    First Class

---
+++ App.js
@@ -0,0 +1,24 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Hola Coder</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+    alignItems: 'center',
+    justifyContent: 'center',
+    
+  },
+  text:{
+    color:'red',
+    fontSize: 65,
+  },
+});
